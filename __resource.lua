90c187b2b90797f66f21344608411d9aef0cd7d4
SATTAGAMES <48390215+SATTAGAMES@users.noreply.github.com>
2019-05-02 07:55:23 -0300

    added fr.lua
    Created Portuguese translation

--- __resource.lua
+++ __resource.lua
@@ -2,13 +2,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Lenzh Chop Shop'
 
-version '1.2.0'
+version '1.0.0'
 
 server_scripts {
 	'@es_extended/locale.lua',
 	'@mysql-async/lib/MySQL.lua',
 	'locales/en.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua',
 }
@@ -16,7 +15,6 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua',
 }
